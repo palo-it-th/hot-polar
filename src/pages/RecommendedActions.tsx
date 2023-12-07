871899698d1ba5728922ea4726b6199fca995a04
Suwijak Chaipipat <schaipipat@palo-it.com>
2023-12-07 17:12:56 +0700

    add: story page & recommended actions

--- src/pages/RecommendedActions.tsx
+++ src/pages/RecommendedActions.tsx
@@ -1,8 +1,53 @@
+import { Button, Card } from "react-bootstrap";
 import Layout from "../components/Layout"
+import './RecommendedActions.css'
+
+const actions = [
+  {
+    name: "Reduce the use of private vehicles",
+    description: "Use public transportation, walk, or cycle instead of driving your own car.",
+    linkName: "Sign for ThaiCAN",
+    link: '#'
+  },
+
+  {
+    name: "Reduce the use of private vehicles",
+    description: "Use public transportation, walk, or cycle instead of driving your own car.",
+    linkName: "Share",
+    link: '#'
+  },
+
+  {
+    name: "Donation",
+    description: "Use public transportation, walk, or cycle instead of driving your own car.",
+    linkName: "Donate",
+    link: '#'
+  },
+
+
+  {
+    name: "Donation",
+    description: "Use public transportation, walk, or cycle instead of driving your own car.",
+    linkName: "Donate",
+    link: '#'
+  }
+]
 
 const RecommendedActions = () => (
   <Layout>
-    <h1>RecommendedActions Page</h1>
+    <h1>Recommended Actions</h1>
+
+    <div className="card-container">
+      {actions.map((action) => (
+        <Card>
+        <Card.Header>{action.name}</Card.Header>
+        <Card.Body>
+          <Card.Text>{action.description}</Card.Text>
+          <Button href={action.link} variant="primary">{action.linkName}</Button>
+        </Card.Body>
+      </Card>
+      ))}
+    </div>
   </Layout>
 )
 
--- src/pages/Story.tsx
+++ src/pages/Story.tsx
@@ -1,8 +1,18 @@
+import { Button, ButtonGroup } from "react-bootstrap"
 import Layout from "../components/Layout"
 
 const Story = () => (
   <Layout>
     <h1>Story Page</h1>
+
+    <p>TO BE ADDED</p>
+
+    <ButtonGroup className="me-2">
+      <Button href="#">Share to your friends</Button>
+      </ButtonGroup> 
+    <ButtonGroup>
+      <Button href="/recommended-actions" className="btn btn-primary">Recommended Actions</Button>
+    </ButtonGroup> 
   </Layout>
 )
 
