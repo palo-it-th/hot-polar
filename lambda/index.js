f8054b2f4fd9dd8382540bb71fb90df0d122dabd
ongpaloit <ptangjintanakarn@palo-it.com>
2023-12-07 16:55:55 +0700

    update to text prompt

--- lambda/index.js
+++ lambda/index.js
@@ -7,8 +7,8 @@ export const handler = async (event = {}, context) => {
     console.log(context)
     // console.log(event.body)
     const data = JSON.parse(event.body)
-    const inputPrompt = data.prompt;
-    console.log(inputPrompt)
+    // const inputPrompt = data.prompt;
+    // console.log(inputPrompt)
     // const {
     //     prompt = "What are the earth's dimensions?",
     //     maxTokens = 300,
@@ -23,17 +23,49 @@ export const handler = async (event = {}, context) => {
     //     stream = false
     // } = event.body;
 
-    const maxTokens = 300;
+    /*
+    *
+    * Human: I want you to come up with a prediction based on the following information in the next 10 years based on air pollution in Thailand. Focus specifically on negative health impacts, social and environmental impacts, and the specific location and quality of life.
+Gender: ${gender}
+Age: ${age}
+Lives in: ${livesIn}
+Occupation: ${occupation}
+Hobby: ${hobby}
+Allergy: ${allergy}
+    * */
+
+    const {
+        gender,
+        age,
+        livesIn,
+        occupation,
+        hobby,
+        allergy
+    } = data
+
+    const constructedPrompt = `I want you to come up with a prediction based on the 
+        following information in the next 10 years based on air pollution in Thailand. 
+        Focus specifically on negative health impacts, social and environmental impacts, 
+        and the specific location and quality of life. 
+        Gender: ${gender}. 
+        Age: ${age}. 
+        Lives in: ${livesIn}. 
+        Occupation: ${occupation}. 
+        Hobby: ${hobby}. 
+        Allergy: ${allergy}
+    `;
+
+    console.log(constructedPrompt);
+
+    const maxTokens = 600;
     const temperature = 1;
     const anthropicVersion = "bedrock-2023-05-31"
     const modelId = 'anthropic.claude-v2:1';
 
     // Modify the prompt to include "Human:" at the beginning and "Assistant:" at the end for Anthropic Claude.
-    const formattedPrompt = `Human: ${inputPrompt}\nAssistant:`;
+    const formattedPrompt = `Human: ${constructedPrompt}\nAssistant:`;
 
     try {
-        // const credentials = await Auth.currentCredentials();
-
         const bedrockruntime = new BedrockRuntimeClient({
             region: 'us-east-1',
             apiVersion: '2023-09-30',
@@ -64,10 +96,6 @@ export const handler = async (event = {}, context) => {
         const data = await bedrockruntime.send(command);
         const decoded = Object.values(data.body).map(code => String.fromCharCode(code)).join('');
         console.log(decoded)
-        // return {
-        //     statusCode: 200,
-        //     decoded
-        // };
         return {
             statusCode: 200,
             body: decoded,
@@ -77,7 +105,16 @@ export const handler = async (event = {}, context) => {
         }
     } catch (error) {
         console.error(error);
-        throw error;
+        // throw error;
+        return {
+            statusCode: 500,
+            body: {
+                message: error.message
+            },
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
     }
 };
 
