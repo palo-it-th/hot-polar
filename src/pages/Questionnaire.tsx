81ed9184ca9cfd3260bfb4ceae5b7c7647de0f40
Suwijak Chaipipat <schaipipat@palo-it.com>
2023-12-07 23:41:41 +0700

    add: generate image

--- src/pages/Questionnaire.tsx
+++ src/pages/Questionnaire.tsx
@@ -1,95 +1,91 @@
-import {Button, Form} from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import Layout from "../components/Layout";
 import './Questionnaire.css';
-import {ChangeEvent, useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import { ChangeEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Questionnaire = () => {
-    const [form, setForm] = useState({
-        gender: "none",
-        age: "",
-        livesIn: "",
-        occupation: "",
-        hobby: "",
-        allergy: ""
+  const [form, setForm] = useState({
+    gender: "none",
+    age: "",
+    livesIn: "",
+    occupation: "",
+    hobby: "",
+    allergy: ""
+  });
+  const navigate = useNavigate();
+
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setForm({
+      ...form,
+      [e.target.name]: value
     });
-    const navigate = useNavigate();
+  };
 
+  const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    setForm({
+      ...form,
+      [e.target.name]: value
+    });
+  }
 
-    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        setForm({
-            ...form,
-            [e.target.name]: value
-        });
-    };
-
-    useEffect(() => {
-        console.log(form)
-    }, [form]);
-
-    const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
-        const value = e.target.value;
-        setForm({
-            ...form,
-            [e.target.name]: value
-        });
+  const handleSubmit = (e: any) => {
+    if (form.gender === "none") {
+      return alert('Please select gender!')
     }
+    e.preventDefault();
+    navigate("/story", {
+      state: {
+        form
+      }
+    })
+  }
 
-    const handleSubmit = (e: any) => {
-        if (form.gender === "none") {
-            return alert('Please select gender!')
-        }
-        e.preventDefault();
-        navigate("/story", {
-            state: {
-                form
-            }
-        })
-    }
+  return (
+    <Layout className="questionnaire">
+      <h1>Do you wish to know what your future looks like living with air pollution?</h1>
+      <p>Please answer a few questions and we will share your future and offer some advice on what you can do.</p>
 
-    return (
-        <Layout className="questionnaire">
-            <h1>Do you wish to know what your future looks like living with air pollution?</h1>
-            <p>Please answer a few questions and we will share your future and offer some advice on what you can do.</p>
-
-            <Form>
-              <Form.Group controlId="formBasicGender">
-                <Form.Label>Gender</Form.Label>
-                <Form.Select name="gender" value={form.gender} onChange={handleSelectChange}>
-                    <option disabled value="none">Please select an option</option>
-                  <option value="Male">Male</option>
-                  <option value="Female">Female</option>
-                  <option value="Secret">Secret</option>
-                </Form.Select>
+      <Form>
+        <Form.Group controlId="formBasicGender">
+          <Form.Label>Gender</Form.Label>
+          <Form.Select name="gender" value={form.gender} onChange={handleSelectChange}>
+            <option disabled value="none">Please select an option</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Secret">Secret</option>
+          </Form.Select>
 
 
-                <Form.Label>Age</Form.Label>
-                <Form.Control name="age" type="number" placeholder="Enter age" required onChange={handleChange}/>
+          <Form.Label>Age</Form.Label>
+          <Form.Control name="age" type="number" placeholder="Enter age" required onChange={handleChange} />
 
 
-                <Form.Label>Lives In</Form.Label>
-                <Form.Control name="livesIn" type="text" placeholder="Enter location" required onChange={handleChange}/>
+          <Form.Label>Lives In</Form.Label>
+          <Form.Control name="livesIn" type="text" placeholder="Enter location" required onChange={handleChange} />
 
 
-                <Form.Label>Occupation</Form.Label>
-                <Form.Control name="occupation" type="text" placeholder="Enter occuation" required onChange={handleChange}/>
+          <Form.Label>Occupation</Form.Label>
+          <Form.Control name="occupation" type="text" placeholder="Enter occuation" required onChange={handleChange} />
 
 
-                <Form.Label>Hobby</Form.Label>
-                <Form.Control name="hobby" type="text" placeholder="Enter hobby" onChange={handleChange}/>
+          <Form.Label>Hobby</Form.Label>
+          <Form.Control name="hobby" type="text" placeholder="Enter hobby" onChange={handleChange} />
 
 
-                <Form.Label>Allergy</Form.Label>
-                <Form.Control name="allergy" type="text" placeholder="Enter allergy" onChange={handleChange}/>
-              </Form.Group>
-            </Form>
+          <Form.Label>Allergy</Form.Label>
+          <Form.Control name="allergy" type="text" placeholder="Enter allergy" onChange={handleChange} />
+        </Form.Group>
+      </Form>
 
-            <Button variant="primary" type="submit" onClick={handleSubmit}>
-              See Your Future!
-            </Button>
-        </Layout>
-    )
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        See Your Future!
+      </Button>
+    </Layout>
+  )
 }
 
 export default Questionnaire
--- src/pages/Story.tsx
+++ src/pages/Story.tsx
@@ -1,53 +1,82 @@
 import { Button, ButtonGroup } from "react-bootstrap"
 import Layout from "../components/Layout"
 import axios from "axios";
-import {useEffect, useState} from "react";
-import {useLocation} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
 import 'react-loading-skeleton/dist/skeleton.css'
 
+const Image = ({ text }: { text: string }) => {
+  const [src, setSrc] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      setLoading(true);
+
+      try {
+        const { data: response } = await axios.post('https://tlxadfyo2k7tlklew6ez3x3zxq0roffe.lambda-url.us-east-1.on.aws/', { text: `${text}, Thai People, Thailand, Unhappy` });
+        setSrc('data:image/png;base64,' + response.image);
+      } catch (error: any) {
+        console.error(error.message);
+      }
+
+      setLoading(false);
+    }
+    fetchImage();
+  }, [])
+
+  return (
+    loading ? <Skeleton height={50} width={50} circle /> : <img src={src} />
+  )
+}
+
 const Story = () => {
-    const { state } = useLocation();
-    const [loading, setLoading] = useState(false);
-    const [paragraphs, setParagraphs] = useState([]);
+  const { state } = useLocation();
+  const [loading, setLoading] = useState(false);
+  const [paragraphs, setParagraphs] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            console.log(state.form)
-            setLoading(true);
-            try {
-                const {data: response} = await axios.post('https://yvaadapva66i7mmveoo7bvni240gpgyi.lambda-url.us-east-1.on.aws/', {
-                    ...state.form
-                });
-                console.log(response)
-                setParagraphs(response.split);
-            } catch (error: any) {
-                console.error(error.message);
-            }
-            setLoading(false);
-        }
-        fetchData();
-    }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
 
-    return (
-        <Layout>
+      try {
+        const { data: response } = await axios.post('https://yvaadapva66i7mmveoo7bvni240gpgyi.lambda-url.us-east-1.on.aws/', {
+          ...state.form
+        });
+        setParagraphs(response.split);
+      } catch (error: any) {
+        console.error(error.message);
+      }
 
-            <h1>Story Page</h1>
-            {loading ? <Skeleton count={25} highlightColor='#757B82'/> :
-                paragraphs.map((text) => (
-                    <p>{text}</p>
-                ))
-            }
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
 
+  return (
+    <Layout>
+      <h1>Your Future</h1>
 
-            <ButtonGroup className="me-2">
-                <Button href="#">Share to your friends</Button>
-            </ButtonGroup>
-            <ButtonGroup>
-                <Button href="/recommended-actions" className="btn btn-primary">Recommended Actions</Button>
-            </ButtonGroup>
-        </Layout>
-    )
+      {loading ? <Skeleton count={25} highlightColor='#757B82' /> :
+        paragraphs.map((text) => (
+          <>
+            <Image text={text} />
+            <p style={{ paddingTop: 10 }}>{text}</p>
+          </>
+        ))
+      }
+
+      <br />
+
+      <ButtonGroup className="me-2">
+        <Button href="#">Share to your friends</Button>
+      </ButtonGroup>
+      <ButtonGroup>
+        <Button href="/recommended-actions" className="btn btn-primary">Recommended Actions</Button>
+      </ButtonGroup>
+    </Layout>
+  )
 }
 
 export default Story
