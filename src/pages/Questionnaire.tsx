3935519b97fe3ba7ef409c3e497b79e318a482cb
ongpaloit <ptangjintanakarn@palo-it.com>
2023-12-07 22:02:35 +0700

    updates to story and questionnaire page: add loading and fetching

--- src/pages/Questionnaire.tsx
+++ src/pages/Questionnaire.tsx
@@ -1,47 +1,89 @@
-import { Button, Form } from "react-bootstrap";
+import {Button, Form} from "react-bootstrap";
 import Layout from "../components/Layout";
 import './Questionnaire.css';
+import {ChangeEvent, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
-const Questionnaire = () => (
-  <Layout className="questionnaire">
-    <h1>Do you wish to know what your future looks like living with air pollution?</h1>
-    <p>Please answer a few questions and we will share your future and offer some advice on what you can do.</p>
-
-    <Form>
-      <Form.Group controlId="formBasicGender">
-        <Form.Label>Gender</Form.Label>
-        <Form.Select>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-          <option value="Secret">Secret</option>
-        </Form.Select>
-
-
-        <Form.Label>Age</Form.Label>
-        <Form.Control type="number" placeholder="Enter age" required />
+const Questionnaire = () => {
+    const [form, setForm] = useState({
+        gender: "",
+        age: "",
+        livesIn: "",
+        occupation: "",
+        hobby: "",
+        allergy: ""
+    });
+    const navigate = useNavigate();
 
 
-        <Form.Label>Lives In</Form.Label>
-        <Form.Control type="text" placeholder="Enter location" required />
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        setForm({
+            ...form,
+            [e.target.name]: value
+        });
+    };
+
+    // useEffect(() => {
+    //     console.log(form)
+    // }, [form]);
+
+    const handleSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        const value = e.target.value;
+        setForm({
+            ...form,
+            [e.target.name]: value
+        });
+    }
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        navigate("/story", {
+            state: form
+        })
+    }
+
+    return (
+        <Layout className="questionnaire">
+            <h1>Do you wish to know what your future looks like living with air pollution?</h1>
+            <p>Please answer a few questions and we will share your future and offer some advice on what you can do.</p>
+
+            <Form>
+              <Form.Group controlId="formBasicGender">
+                <Form.Label>Gender</Form.Label>
+                <Form.Select name="gender" value={form.gender} onChange={handleSelectChange}>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                  <option value="Secret">Secret</option>
+                </Form.Select>
 
 
-        <Form.Label>Occupation</Form.Label>
-        <Form.Control type="text" placeholder="Enter occuation" required />
+                <Form.Label>Age</Form.Label>
+                <Form.Control name="age" type="number" placeholder="Enter age" required onChange={handleChange}/>
 
 
-        <Form.Label>Hobby</Form.Label>
-        <Form.Control type="text" placeholder="Enter hobby" />
+                <Form.Label>Lives In</Form.Label>
+                <Form.Control name="livesIn" type="text" placeholder="Enter location" required onChange={handleChange}/>
 
 
-        <Form.Label>Allergy</Form.Label>
-        <Form.Control type="text" placeholder="Enter allergy" />
-      </Form.Group>
-    </Form>
+                <Form.Label>Occupation</Form.Label>
+                <Form.Control name="occupation" type="text" placeholder="Enter occuation" required onChange={handleChange}/>
 
-    <Button href="/story" variant="primary" type="submit">
-      See Your Future!
-    </Button>
-  </Layout>
-)
+
+                <Form.Label>Hobby</Form.Label>
+                <Form.Control name="hobby" type="text" placeholder="Enter hobby" onChange={handleChange}/>
+
+
+                <Form.Label>Allergy</Form.Label>
+                <Form.Control name="allergy" type="text" placeholder="Enter allergy" onChange={handleChange}/>
+              </Form.Group>
+            </Form>
+
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+              See Your Future!
+            </Button>
+        </Layout>
+    )
+}
 
 export default Questionnaire
--- src/pages/Story.tsx
+++ src/pages/Story.tsx
@@ -1,19 +1,52 @@
 import { Button, ButtonGroup } from "react-bootstrap"
 import Layout from "../components/Layout"
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {useLocation} from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
+import 'react-loading-skeleton/dist/skeleton.css'
 
-const Story = () => (
-  <Layout>
-    <h1>Story Page</h1>
+const Story = () => {
+    const { state } = useLocation();
+    const [loading, setLoading] = useState(false);
+    const [paragraphs, setParagraphs] = useState([]);
 
-    <p>TO BE ADDED</p>
+    useEffect(() => {
+        const fetchData = async () =>{
+            setLoading(true);
+            try {
+                const {data: response} = await axios.post('https://yvaadapva66i7mmveoo7bvni240gpgyi.lambda-url.us-east-1.on.aws/', {
+                    ...state.form
+                });
+                console.log(response)
+                setParagraphs(response.split);
+            } catch (error: any) {
+                console.error(error.message);
+            }
+            setLoading(false);
+        }
+        fetchData();
+    }, []);
 
-    <ButtonGroup className="me-2">
-      <Button href="#">Share to your friends</Button>
-      </ButtonGroup> 
-    <ButtonGroup>
-      <Button href="/recommended-actions" className="btn btn-primary">Recommended Actions</Button>
-    </ButtonGroup> 
-  </Layout>
-)
+    return (
+        <Layout>
+
+            <h1>Story Page</h1>
+            {loading ? <Skeleton count={25} highlightColor='#757B82'/> :
+                paragraphs.map((text) => (
+                    <p>{text}</p>
+                ))
+            }
+
+
+            <ButtonGroup className="me-2">
+                <Button href="#">Share to your friends</Button>
+            </ButtonGroup>
+            <ButtonGroup>
+                <Button href="/recommended-actions" className="btn btn-primary">Recommended Actions</Button>
+            </ButtonGroup>
+        </Layout>
+    )
+}
 
 export default Story
