227444d64381a11a73cee416f15cb5ff1baedb0e
ongpaloit <ptangjintanakarn@palo-it.com>
2023-12-07 22:19:59 +0700

    update index file to use useNavigate

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,18 @@
 import { Button } from "react-bootstrap"
 import Layout from "../components/Layout"
 import './Index.css'
+import {useNavigate} from "react-router-dom";
 
-const Index = () =>
-  <Layout className="index">
-    <img src="/assets/logo.png" alt="logo" />
-    <h1>Your Future with Air Pollution</h1>
-    <p>Average life reduction of Thai population is 2 years!</p>
-    <Button href="/questionnaire" variant="primary">Get Started!</Button>
-  </Layout>
+const Index = () => {
+    const navigate = useNavigate();
+    return (
+        <Layout className="index">
+            <img src="/assets/logo.png" alt="logo"/>
+            <h1>Your Future with Air Pollution</h1>
+            <p>Average life reduction of Thai population is 2 years!</p>
+            <Button variant="primary" onClick={() => navigate('questionnaire')}>Get Started!</Button>
+        </Layout>
+    )
+}
 
 export default Index
